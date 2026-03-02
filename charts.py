dabcd2b96e8d43318b184454165f2348357e85d9
vavlxxx <vladimir.nefedov357@gmail.com>
2026-03-02 11:37:31 +0500

    added sounds + corrected zones

--- charts.py
+++ charts.py
@@ -1,28 +1,36 @@
-﻿import pyqtgraph as pg
-from PyQt6.QtWidgets import QVBoxLayout, QWidget
+import pyqtgraph as pg
+from PyQt6.QtWidgets import QGroupBox, QHBoxLayout, QVBoxLayout, QWidget
 
 
 class ZoneChartsWidget(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        layout = QVBoxLayout(self)
+        layout = QHBoxLayout(self)
         layout.setContentsMargins(8, 8, 8, 8)
-        layout.setSpacing(8)
+        layout.setSpacing(10)
+
+        left_group = QGroupBox("Температура")
+        right_group = QGroupBox("Дымность")
+        left_layout = QVBoxLayout(left_group)
+        right_layout = QVBoxLayout(right_group)
+        left_layout.setContentsMargins(8, 8, 8, 8)
+        right_layout.setContentsMargins(8, 8, 8, 8)
 
         self.temp_plot = pg.PlotWidget()
         self.smoke_plot = pg.PlotWidget()
 
-        self._configure_plot(
-            self.temp_plot, "Температура", "Время, с", "Температура, °C"
-        )
+        self._configure_plot(self.temp_plot, "Температура", "Время, с", "Температура, °C")
         self._configure_plot(self.smoke_plot, "Дымность", "Время, с", "Дым, ppm")
 
         self.temp_curve = self.temp_plot.plot(pen=pg.mkPen("#f57c00", width=2))
         self.smoke_curve = self.smoke_plot.plot(pen=pg.mkPen("#1565c0", width=2))
 
-        layout.addWidget(self.temp_plot)
-        layout.addWidget(self.smoke_plot)
+        left_layout.addWidget(self.temp_plot)
+        right_layout.addWidget(self.smoke_plot)
+
+        layout.addWidget(left_group, 1)
+        layout.addWidget(right_group, 1)
 
     def _configure_plot(self, plot, title, x_label, y_label):
         plot.setBackground("#ffffff")
